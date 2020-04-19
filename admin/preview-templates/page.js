5fa220de9774f2448589235f75c254d0386d7650
Ben Kammerling <bkammerling@googlemail.com>
2020-04-19 22:13:33 +0200

    cms preview edits

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -1,31 +1,21 @@
 import htm from "https://unpkg.com/htm?module";
 
-const html = htm.bind(h);
-
 // Preview component for a Page
 const Page = createClass({
-  render() {
-    const entry = this.props.entry;
-
-    return html`
-      <main>
-        <h1>${entry.getIn(["data", "title"], null)}</h1>
-        <h2>${entry.getIn(["data", "subtitle"], null)}</h2>
-
-        ${this.props.widgetFor("body")}
-
-        ${this.props.widgetsFor("page_sections").map(function(section, index) {
-          return `<div><h3>${section.title}</h3>
-          ${section.cols.map(function(col, index) {
-            return col.body;
-          })}
-          </div>
-          `
-        })}
-
-      </main>
-    `;
-  }
+  render: function() {
+      var entry = this.props.entry;
+      return h('div', {},
+        h('h3', {}, entry.getIn(['data', 'title'])),
+        h('h1', {}, entry.getIn(['data', 'subtitle'])),
+        h('div', {"className": "text"}, this.props.widgetFor('body')),
+        this.props.widgetsFor('page_sections').map(function(section, index) {
+        return h('div', {key: index},
+          h('hr', {}),
+          h('strong', {}, section.getIn(['data', 'title']))
+        );
+      })
+      );
+    }
 });
 
 export default Page;
