eb329dd72628754b1935b73e1695d249dac0bb04
Ben Kammerling <bkammerling@googlemail.com>
2020-04-19 23:06:56 +0200

    preview fix i think

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -15,7 +15,7 @@ const Page = createClass({
           h('div', {"className": "text"}, this.props.widgetFor(section.getIn(['data', 'prebody']))),
           section.getIn(['data', 'cols']).map(function(col, index) {
             return h('div', {}, this.props.widgetFor(col));
-          }),
+          }, this)),
           h('div', {}, this.props.widgetFor(section.getIn(['data', 'postbody'])))
         );
       })
